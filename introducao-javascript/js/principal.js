155a2667d60113221274ab70dd1155fd4f4c761a
kayque de moraes urbano oliveira <kayquedemorais13@gmail.com>
2021-03-11 03:49:40 -0300

    add

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,32 +1,61 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+// Validação do peso e altura
+for (let i = 0; i < pacientes.length; i++) {
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var tdImc = paciente.querySelector(".info-imc");
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido";
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        paciente.classList.add("campo-invalido");
+        tdImc.textContent = "Peso inválido";
+        
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        paciente.classList.add("campo-invalido");
+        tdImc.textContent = "Altura inválida";
+        
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+        
+    }
+
 }
+// Inserção de um novo paciente
+var nomeNovo = document.querySelector("#nome").value;
+var pesoNovo = document.querySelector("#peso").value;
+var alturaNova = document.querySelector("#altura").value;
+var gorduraNova = document.querySelector("#gordura").value;
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida";
-}
+var botao = document.querySelector("#adicionar-paciente");
 
-if (pesoEhValido && alturaEhValida) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
+botao.addEventListener( "click" , function(event){
+    event.defaultPrevented();
+
+} 
+);
+
+
+//tabela-pacientes -- add tr
+
+
